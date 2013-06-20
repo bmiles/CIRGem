285552b7ce6a235a284748f1e90b17ade9e89cfe
Ben Miles <ben.miles2@gmail.com>
2013-06-20 18:17:31 +0100

    added description of gem

--- lib/CIRGem.rb
+++ lib/CIRGem.rb
@@ -1,5 +1,34 @@
 require "CIRGem/version"
 
 module CIRGem
-  # Your code goes here...
+
+  class Chemical
+    include HTTParty
+    
+    base_uri "cactus.nci.nih.gov/chemical/structure/"
+    
+    attr_accessor :name, :stdinchikey, :smiles
+    
+    def initialize(name, stdinchikey=nil, smiles=nil)
+      @name = name
+      @stdinchikey = name_to_stdinchikey()
+      @smiles = name_to_smiles()
+    end
+  
+    def name_to_stdinchikey()
+      response = self.class.get("/#{@name.gsub(/\s+/, "")}/stdinchikey")
+      if response.parsed_response =~ /.*=(.*)/
+        @stdinchikey = $1
+      else
+        puts "can't parse strucutre"  
+      end
+    end
+    
+    def name_to_smiles()
+      response = self.class.get("/#{@name.gsub(/\s+/, "")}/smiles")
+      @smiles = response.parsed_response
+    end
+    
+  end
+
 end
