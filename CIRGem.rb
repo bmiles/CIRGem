8bb1821303292737e3a8f218f1cc36c87ae5b4a9
Ben Miles <ben.miles2@gmail.com>
2013-06-20 17:38:18 +0100

    added name to smiles method

--- CIRGem.rb
+++ CIRGem.rb
@@ -3,19 +3,26 @@ class Chemical
   
   base_uri "cactus.nci.nih.gov/chemical/structure/"
   
-  attr_accessor :structure_identifier, :stdinchikey
+  attr_accessor :name, :stdinchikey, :smiles
   
-  def initialize(structure_identifier, stdinchikey=nil)
-    @structure_identifier = structure_identifier
-    @stdinchikey = nil
+  def initialize(name, stdinchikey=nil, smiles=nil)
+    @name = name
+    @stdinchikey = stdinchikey
+    @smiles = smiles
   end
 
-  def inchikey()
-    response = self.class.get("/#{@structure_identifier}/stdinchikey")
+  def name_to_stdinchikey()
+    response = self.class.get("/#{@structure_identifier.gsub(/\s+/, "")}/stdinchikey")
     if response.parsed_response =~ /.*=(.*)/
       @stdinchikey = $1
     else
       puts "can't parse strucutre"  
     end
   end
+  
+  def name_to_smiles()
+    response = self.class.get("/#{@structure_identifier.gsub(/\s+/, "")}/smiles")
+    @smiles = response.parsed_response
+  end
+  
 end
